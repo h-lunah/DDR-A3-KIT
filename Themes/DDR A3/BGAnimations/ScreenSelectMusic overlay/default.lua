e2da3d2e777634be2d30e47ade1d3d9748702ce3
Piotr Zalewski <bardzoglodnypiotrek@gmail.com>
2025-05-08 21:55:33 +0200

    fix: remove shock arrow marker from radar, don't stop random BPM tween in OffCommand

--- Themes/DDR A3/BGAnimations/ScreenSelectMusic overlay/default.lua
+++ Themes/DDR A3/BGAnimations/ScreenSelectMusic overlay/default.lua
@@ -1,7 +1,6 @@
 local diff = Def.ActorFrame{};
 local Radar = Def.ActorFrame{};
 local Target = Def.ActorFrame{};
-local Shock = Def.ActorFrame{};
 
 for _,pn in pairs(GAMESTATE:GetEnabledPlayers()) do
 	diff[#diff+1] = loadfile(THEME:GetPathB("ScreenSelectMusic","overlay/Difficulty"))(pn)..{
@@ -11,9 +10,6 @@ for _,pn in pairs(GAMESTATE:GetEnabledPlayers()) do
 		InitCommand=function(s) s:xy(pn==PLAYER_1 and SCREEN_LEFT+86 or SCREEN_RIGHT-86,_screen.cy+24):zoom(0.667) end,
 	}
 	Target[#Target+1] = loadfile(THEME:GetPathB("ScreenSelectMusic","overlay/TargetScore/default.lua"))(pn);
-	Shock[#Shock+1] = loadfile(THEME:GetPathB("ScreenSelectMusic","overlay/ShockArrows/default.lua"))(pn)..{
-		InitCommand=function(s) s:xy(pn==PLAYER_1 and SCREEN_LEFT+86 or SCREEN_RIGHT-86,_screen.cy+36):zoom(0.667) end,
-	};
 end
 
 local TwoPart = Def.ActorFrame{
@@ -25,7 +21,6 @@ local TwoPart = Def.ActorFrame{
 };
 
 return Def.ActorFrame{
-	OffCommand=function(s) s:finishtweening() end,
 	TwoPart;
 	loadfile(THEME:GetPathB("ScreenSelectMusic","overlay/Info"))()..{
 		OnCommand=function(s) s:zoom(0.667):y(57):diffusealpha(0):sleep(0.4):linear(0.05):diffusealpha(0.75):linear(0.1):diffusealpha(0.25):linear(0.1):diffusealpha(1) end,
@@ -36,5 +31,4 @@ return Def.ActorFrame{
 	diff;
 	Target;
 	Radar;
-	Shock;
 }
